03f11cd8fb6ef3c8659f06d1f983c8e63cdb5107
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-08-05 16:25:49 +0200

    This should be done

---
+++ src/test/java/com/company/taskspringboot/TaskspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.taskspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
